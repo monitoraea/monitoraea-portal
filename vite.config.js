05570dbb84fded470c1844ebe53227f725df78e4
Ricardo Zylbergeld <ricardo@digitao.com.br>
2024-11-21 17:58:55 +0200

    no more sass warnings

--- vite.config.js
+++ vite.config.js
@@ -15,4 +15,11 @@ export default defineConfig({
             },
         },
     },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                api: 'modern' // or "modern-compiler"
+            }
+        }
+    },
 })
