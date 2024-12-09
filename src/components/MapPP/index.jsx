aa685a2091a7ffb28d64fa1cf9d30c81630f2a86
Ricardo Zylbergeld <ricardo@digitao.com.br>
2024-12-09 11:31:00 +0200

    ppea home

--- src/components/MapPP/index.jsx
+++ src/components/MapPP/index.jsx
@@ -6,6 +6,8 @@ import { Map, TileLayer, WMSTileLayer/* , Popup */, ZoomControl } from 'react-le
 import ToggleOn from '../../components/icons/check-circle.svg?react';
 import ToggleOff from '../../components/icons/slash.svg?react';
 
+import DashExample from '../../images/ppea-dash-example.png'
+
 import styles from './styles.module.scss';
 
 const mapRef = createRef();
@@ -23,64 +25,100 @@ export default function MapPP() {
     ])
 
     const doToggle = (id) => {
-        if(recortes.includes(id)) _recortes(r => r.filter(r => r !== id));
+        if (recortes.includes(id)) _recortes(r => r.filter(r => r !== id));
         else _recortes([...recortes, id]);
     }
 
-    return (<section id="mapa">
-        {/* <div className="map-header">
-            <div className="title-xl">Mapeamento de Iniciativas Monitoradas</div>
-            <div className="map-stats">
-                <div className="stat">
-                    <div className="title">Políticas</div>
-                    <div className="number">413</div>
-                </div>
-            </div>
-        </div> */}
-        <div className={styles.container}>
-            <div className={styles['map-container']}>
-                <Map center={position} zoomControl={false} zoom={zoom} ref={mapRef} maxZoom={18} minZoom={3} scrollWheelZoom={false} /*  onClick={handleMapClick} */>
-                    <TileLayer
-                        attribution='<a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    />
+    return (<>
+        <section className={styles['ppea-dash']}>
 
-                    <WMSTileLayer
-                        url={import.meta.env.VITE_GEOSERVER_URL}
-                        layers="pppzcm:published_ppeas_special"
-                        format="image/png"
-                        transparent={true}
-                        opacity={0.8}
-                        cql_filter={recortes ? `recorte in (${recortes.map(r => `'${r}'`).join(',')})` : ''}
-                    />
+            <div className="width-limiter">
 
-                    <ZoomControl position="bottomright" />
-                </Map>
-            </div>
+                <div className={styles['ppea-dash-inner']}>
 
-            <div className={`p-4 ${styles.filter_panel}`}>
+                    <div className={styles.left}>
+                        <div className={styles['title']}>Políticas Públicas de<br />Educação Ambiental no Brasil</div>
 
-                <div className="row">
-                    <div className={`col-md-4 ${styles.chooser}`}>
-                        {[
-                            ['uf','Unidades Federativas'],
-                            ['municipio','Municípios'],
-                            ['p_urbano','Perímetros Urbanos'],
-                            ['bairro','Bairros'],
-                            ['u_conservacao','Unidades de Conservação'],
-                            ['bacia','Outros recortes'],
-                        ].map(r=><Toggler key={r[0]} title={r[1]} on={recortes.includes(r[0])} onToggle={()=>doToggle(r[0])}/>)}
+                        <div className={styles['big-numbers']}>
+                            <div className={styles['box-with-image']}>
+                                <div className={`${styles['box']} ${styles['box-1']}`}>
+                                    <div className={styles.number}>228</div>
+                                    <div className={styles.text}>Políticas Públicas de EA</div>
+                                </div>
+                            </div>
+
+                            <div className={styles['box-with-image']}>
+                                <div className={`${styles['box']}`}>
+                                    <div className={styles.number}>XXX</div>
+                                    <div className={styles.text}>Instituições</div>
+                                </div>
+                            </div>
+
+                            <div className={styles['box-with-image']}>
+                                <div className={`${styles['box']}`}>
+                                    <div className={styles.number}>XXX</div>
+                                    <div className={styles.text}>Pessoas</div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
+
+                    <div className={styles.right}>
+                        <img src={DashExample} />
+                    </div>
+
                 </div>
 
             </div>
-        </div>
-    </section>)
+        </section>
+
+        <section id="mapa">
+
+            <div className={styles.container}>
+                <div className={styles['map-container']}>
+                    <Map center={position} zoomControl={false} zoom={zoom} ref={mapRef} maxZoom={18} minZoom={3} scrollWheelZoom={false} /*  onClick={handleMapClick} */>
+                        <TileLayer
+                            attribution='<a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+
+                        <WMSTileLayer
+                            url={import.meta.env.VITE_GEOSERVER_URL}
+                            layers="pppzcm:published_ppeas_special"
+                            format="image/png"
+                            transparent={true}
+                            opacity={0.8}
+                            cql_filter={recortes ? `recorte in (${recortes.map(r => `'${r}'`).join(',')})` : ''}
+                        />
+
+                        <ZoomControl position="bottomright" />
+                    </Map>
+                </div>
+
+                <div className={`p-4 ${styles.filter_panel}`}>
+
+                    <div className="row">
+                        <div className={`col-md-4 ${styles.chooser}`}>
+                            {[
+                                ['uf', 'Unidades Federativas'],
+                                ['municipio', 'Municípios'],
+                                ['p_urbano', 'Perímetros Urbanos'],
+                                ['bairro', 'Bairros'],
+                                ['u_conservacao', 'Unidades de Conservação'],
+                                ['bacia', 'Outros recortes'],
+                            ].map(r => <Toggler key={r[0]} title={r[1]} on={recortes.includes(r[0])} onToggle={() => doToggle(r[0])} />)}
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+    </>)
 
 }
 
 function Toggler({ title, on, onToggle }) {
-    return (<div onClick={()=>onToggle()}>
+    return (<div onClick={() => onToggle()}>
         <div>{title}</div>
         {on && <ToggleOn />}
         {!on && <ToggleOff />}
--- src/pages/PPEA/PPEA.jsx
+++ src/pages/PPEA/PPEA.jsx
@@ -11,54 +11,101 @@ import { content_types } from '../../utils';
 import MapPPEA from '../../components/MapPP';
 
 import './style.scss';
+import styles from './styles.module.scss'
+
+import pa_sep from '../../images/pa-sep.png';
+import d1 from '../../images/diagram-1.png';
+import d2 from '../../images/diagram-2.png';
+import d3 from '../../images/diagram-3.png';
 
 function PPEA() {
 
   return (
     <>
-        <Header />
+      <Header />
 
-        <section id="sobre">
+      <section id="sobre">
         <div className="width-limiter">
-          <p className="p-xl"><DynamicContent keyRef="pp.intro" /></p>
+
+          <div className="ppea-about">
+
+            <div className="pa-left">
+              <div className='pa-title'>Politicas Públicas de Educação Ambiental</div>
+              <div>
+                <p>
+                  O Sistema MonitoraEA adota a abordagem de políticas públicas <span>policêntricas, multicêntricas ou redes de políticas</span>, as quais são frutos de regras institucionais (arranjos institucionais) que envolvem a diversidade de atores para a resolução de um problema público comum que deve ser enfrentado pela coletividade.
+                </p>
+                <p>
+                  Nesta abordagem, o ator protagonista de uma política pública pode ser tanto o <span>governo</span>, como a <span>sociedade civil</span> ou <span>setor privado</span> que juntos formulam e implementam políticas públicas defendendo o que traz benefícios para todos sem excluir ninguém.
+                </p>
+
+              </div>
+            </div>
+
+            <div className='pa-sep'><img src={pa_sep} /></div>
+
+            <div className="pa-right">
+              <p>
+                Com essa abordagem de políticas públicas são considerados os diversos centros de tomadas de decisão capilarizados nos territórios, no entanto, os atores são interdependentes já que nenhum deles possui recursos ou autoridade por completo para resolver sozinho o problema público.
+              </p>
+              <p>
+                É preciso <span>diálogo e cooperação em rede</span> o que exige o desafio da coordenação entre os participantes e governança democrática, participativa e colaborativa.
+              </p>
+            </div>
+          </div>
+
         </div>
-      </section>
-      
+
+      </section >
+
       <MapPPEA />
 
-      
-      {['news'/* ,'learning' */].map(c => <ContentByType
-        key={c}
-        id={content_types[c][2]}
-        contentType={c}
-        title={content_types[c][1]}
-        moreText={content_types[c][3]}
-        portal="pp"
-      />)}
+      <div className={styles['diagrams']}>
+        <div className={styles.d1}>
+          <img src={d1} />
+        </div>
+        <div className={styles.d2}>
+          <img src={d2} />
+        </div>
 
-      <Faq portal="pp" />      
-      
-      <ContentByType
-        id={content_types['publication'][2]}
-        contentType="publication"
-        title={content_types['publication'][1]}
-        moreText={content_types['publication'][3]}
-        portal="pp"
-      />
+        <div className={styles.d3}>
+          <div className="width-limiter">
 
-      <section id="conselho">
-        <div className="width-limiter">
-          <div className="section-header">
-            <div className="section-title">Conselho Institucional</div>
-          </div>
-          <div className="logos">
-            <img src={imgplaceholder} alt="" className="logo" />
-            <img src={imgplaceholder} alt="" className="logo" />
-            <img src={imgplaceholder} alt="" className="logo" />
-            <img src={imgplaceholder} alt="" className="logo" />
+            <div className={styles['title-box']}>
+              Indicadores de Monitoramento e Avaliação de PPEA
+            </div>
+
+            <div className={styles.texto}>
+              <p>
+                Os indicadores de monitoramento e avaliação de políticas públicas de educação ambiental (PPEA) foram construídos de maneira participativa entre os anos de 2016 a 2019, em um processo que envolveu atores do campo da EA de todas as regiões do Brasil, e de todos os segmentos de atuação.
+                A descrição completa do processo metodológico de construção e validação dos indicadores pode ser encontrada no <span>caderno de Indicadores de M&A de PPEA</span> e no livro <span>Avaliação e monitoramento de políticas públicas de educação ambiental no Brasil: transição para sociedades sustentáveis.</span>
+              </p>
+              <p>
+                Os indicadores são organizados a partir de 8 dimensões - que constituem o marco referencial de monitoramento e avaliação de PPEA. O atendimento à cada dimensão é verificado pelo conjunto de indicadores à elas associados.
+              </p>
+            </div>
+
+            <div className={styles.diagrama}>
+              <img src={d3} />
+            </div>
           </div>
         </div>
-      </section>
+
+      </div>
+
+
+      {
+        ['news'/* ,'learning' */].map(c => <ContentByType
+          key={c}
+          id={content_types[c][2]}
+          contentType={c}
+          title={content_types[c][1]}
+          moreText={content_types[c][3]}
+          portal="pp"
+        />)
+      }
+
+      <Faq portal="pp" />
 
     </>
   );
