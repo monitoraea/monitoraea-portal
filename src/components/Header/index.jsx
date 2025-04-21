464bb3a01e4c7c211d64b343a10371f5eeecf583
Ricardo Zylbergeld <ricardo@digitao.com.br>
2025-04-21 10:08:06 +0300

    home - instagram (only) social network

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,27 +1,40 @@
-import Nav from '../nav/nav';
+import Nav from "../nav/nav";
 
-import bg from '../../images/bg_top.jpg'
-import miolo from '../../images/miolo_top.png'
-import youtube from '../../images/youtube.png'
-import instagram from '../../images/instagram.png'
+import bg from "../../images/bg_top.jpg";
+import miolo from "../../images/miolo_top.png";
+import youtube from "../../images/youtube.png";
+import instagram from "../../images/instagram.png";
 
 export default function Header() {
-    return (<>
+  return (
+    <>
+      <div className="banner">
+        <img className="bg" src={bg} alt="" />
+        <div className="backdrop"></div>
+        <div className="content">
+          <div className="title">
+            <img src={miolo} alt="" />
+          </div>
 
-        <div className="banner">
-            <img className="bg" src={bg} alt="" />
-            <div className="backdrop"></div>
-            <div className="content">
-                <div className="title">
-                    <img src={miolo} alt="" />
-                </div>
-
-                <div className="right">
-                    <div className="social-media"><img src={youtube} alt="" /><img src={instagram} alt="" /></div>
-                    <button onClick={() => window.location.href = '/colabora'} className="login">Acessar</button>
-                </div>
+          <div className="right">
+            <div className="social-media">
+              <a
+                href="https://www.instagram.com/anppea_monitoraea/"
+                target="_blank"
+              >
+                <img src={instagram} alt="" />
+              </a>
             </div>
+            <button
+              onClick={() => (window.location.href = "/colabora")}
+              className="login"
+            >
+              Acessar
+            </button>
+          </div>
         </div>
-        <Nav />
-    </>)
+      </div>
+      <Nav />
+    </>
+  );
 }
