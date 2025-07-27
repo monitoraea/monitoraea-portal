839782e8d7519a247236f3d9745dbe5ff28e412a
Ricardo Zylbergeld <ricardo@digitao.com.br>
2025-07-27 15:33:35 +0300

    educom_clima map

--- src/components/MapEDUCLI/index.jsx
+++ src/components/MapEDUCLI/index.jsx
@@ -43,29 +43,50 @@ function prepareFilters(filters, togglers) {
     let preparedFilters = '';
 
     for (let filter in filters) {
-        if (filters[filter] && togglers[filter]) preparedFilters = `${preparedFilters}&f_${filter}=${filters[filter]}`;
+        if (filter === 'ids' || (filters[filter] && togglers[filter])) preparedFilters = `${preparedFilters}&f_${filter}=${filters[filter]}`;
     }
 
     return preparedFilters;
 }
 
-async function getUFs(filters, togglers) {
-    const { data } = await axios.get(`${import.meta.env.VITE_SERVER}commission/ufs?none=1${prepareFilters(filters, togglers)}`);
+async function getRegioes(urlFilters) {
+    const { data } = await axios.get(`${import.meta.env.VITE_SERVER}educom_clima/regions?${urlFilters}`);
 
     return data;
 }
 
+async function getUFs(urlFilters) {
+    const { data } = await axios.get(`${import.meta.env.VITE_SERVER}educom_clima/ufs?${urlFilters}`);
+
+    return data;
+}
+
+const initialFieldsState = {
+  regioes: null,
+  ufs: null,
+  id: null,
+}
+
+const initialTogglersState = {
+  regioes: false,
+  municipios: false,
+  id: false,
+};
+
 export default function MapPP() {
     const [iniciativas, _iniciativas] = useState(null);
-    const [iniciativas_ids, _iniciativas_ids] = useState(null);
+    const [uf_ids, _uf_ids] = useState(null);
+    const [clicked_ufs, _clicked_ufs] = useState(null);
 
     const [regioes, _regioes] = useState(null);
     const [ufs, _ufs] = useState(null);
     const [loading, _loading] = useState(true);
 
     const [page, _page] = useState(1);
+    const [limit, _limit] = useState(6);
 
     const [consultas_open, _consultas_open] = useState(false);
+    const [tab, _tab] = useState('filters');
 
     const [filters, _filters] = useState({});
     const [fields, _fields] = useState({
@@ -81,13 +102,11 @@ export default function MapPP() {
     const [bbox, _bbox] = useState(null)
     const [selected, _selected] = useState(null)
 
-    useEffect(() => {
-        getOptions();
-    }, []);
+    const [urlFilters, _urlFilters] = useState('');
 
     useEffect(() => {
-        console.log({iniciativas_ids});
-    }, [iniciativas_ids]);
+        console.log({ uf_ids });
+    }, [uf_ids]);
 
     // useEffect(() => {
     //    if(mapRef.current?.leafletElement) {
@@ -112,6 +131,11 @@ export default function MapPP() {
     }, [bbox])
 
     useEffect(() => {
+        // _urlFilters
+        _urlFilters(prepareFilters(filters, togglers))
+    }, [filters, togglers])
+
+    useEffect(() => {
         let isFiltered = false;
         for (let f of Object.values(filters)) {
             if (!!f) {
@@ -123,18 +147,19 @@ export default function MapPP() {
     }, [filters])
 
     useEffect(() => {
-        async function fetchData(page = 1, filters) {
+        async function fetchData(page = 1) {
+
             _loading(true);
             const {
                 data,
-            } = await axios.get(`${import.meta.env.VITE_SERVER}commission/?limit=6&page=${page}${prepareFilters(filters, togglers)}`);
+            } = await axios.get(`${import.meta.env.VITE_SERVER}educom_clima/formap/?limit=${limit}&page=${page}?${urlFilters}`);
             _loading(false);
 
             _iniciativas(data);
         }
 
-        fetchData(page, filters);
-    }, [page, filters, togglers]);
+        fetchData(page);
+    }, [page, urlFilters]);
 
     useEffect(() => {
         _page(1);
@@ -142,27 +167,31 @@ export default function MapPP() {
 
         async function fetchGeoData() {
             if (Object.keys(filters).filter(k => !!filters[k]).length === 0) {
-                _iniciativas_ids(null);
+                _uf_ids(null);
                 return;
             }
 
-            const { data } = await axios.get(`${import.meta.env.VITE_SERVER}commission/geo/?${prepareFilters(filters, togglers)}`);
+            const { data } = await axios.get(`${import.meta.env.VITE_SERVER}educom_clima/geo/?${urlFilters}`);
 
-            _iniciativas_ids(data);
+            _uf_ids(data);
         }
 
         /* reset zoom and position */
         mapRef && mapRef.current && mapRef.current.leafletElement.setView(position, zoom);
         fetchGeoData(filters);
-    }, [filters, togglers]);
+    }, [urlFilters]);
 
     useEffect(() => {
+        async function fetchRegioes() {
+            _regioes(await getRegioes(urlFilters));
+        }
         async function fetchUFs() {
-            _ufs(await getUFs(filters, togglers));
+            _ufs(await getUFs(urlFilters));
         }
 
+        fetchRegioes();
         fetchUFs();
-    }, [filters, togglers]);
+    }, [urlFilters]);
 
     useEffect(() => {
         if (!selected) return;
@@ -175,15 +204,9 @@ export default function MapPP() {
     const handleSelect = (p) => {
         _selected(p.id)
         _bbox(p.bbox)
+
+        console.log(p.bbox)
     }
-
-    const getOptions = async () => {
-        const {
-            data: { regioes },
-        } = await axios.get(`${import.meta.env.VITE_SERVER}commission/options`);
-
-        _regioes(regioes);
-    };
 
     const onFilterChange = (type, selectedOption) => {
         _page(1);
@@ -257,6 +280,82 @@ export default function MapPP() {
         _togglers(togglers => ({ ...togglers, [filter]: checked }))
     }
 
+    async function getClickedFeatureId(_map, latlng) {
+        const _url = import.meta.env.VITE_GEOSERVER_URL;
+
+        // Construct a GetFeatureInfo request URL given a point
+        var point = _map.latLngToContainerPoint(latlng, _map.getZoom()),
+            size = _map.getSize(),
+            params = {
+                request: 'GetFeatureInfo',
+                service: 'WMS',
+                srs: 'EPSG:4674',
+                transparent: true,
+                version: '1.1.1',
+                format: 'image/png',
+                bbox: _map.getBounds().toBBoxString(),
+                height: size.y,
+                width: size.x,
+                layers: 'pppzcm:educom_clima_count',
+                query_layers: 'pppzcm:educom_clima_count',
+                info_format: 'application/json',
+                x: Math.round(point.x),
+                y: Math.round(point.y),
+                feature_count: 20,
+            };
+
+        const getFeatureInfoURL = _url + L.Util.getParamString(params, _url, true);
+
+        /* console.log(getFeatureInfoURL) */
+
+        return await new Promise((resolve, reject) =>
+            fetch(getFeatureInfoURL)
+                .then(response => response.json())
+                .then(data => {
+                    const err = typeof data === 'object' ? null : data;
+                    if (err) reject(err);
+                    else if (!data.features.length) resolve(null);
+                    else {
+                        const [table] = data.features[0].id.split('.');
+                        const ids = data.features.map(f => f.properties.cd_uf);
+
+                        resolve({
+                            table,
+                            ids: [...new Set(ids)], // remove duplicates,
+                        });
+                    }
+                }),
+        );
+    }
+
+    const handleMapClick = async e => {
+        const data = await getClickedFeatureId(mapRef.current.leafletElement, e.latlng);
+
+        if (!data) {
+            return;
+        }
+
+        const { ids } = data;
+
+        // /* filtrar pelos projetos ativos */ ?????????????????????????????????????
+        // const projectsToSee = projects_ids ? ids.filter(id => projects_ids.includes(id)) : ids;
+
+        console.log({ ids })
+
+        _tab('clicked');
+        _consultas_open(true);
+        _filters({ ids: ids.join(',') });
+        _clicked_ufs(ids)
+    };
+
+    const closeClickedTab = () => {
+        _filters({});
+        _clicked_ufs(null)
+        _fields(initialFieldsState);
+        _togglers(initialTogglersState);
+        _tab('filters');
+    }
+
     return (<>
         <section className={styles['ciea-dash']}>
 
@@ -264,7 +363,7 @@ export default function MapPP() {
 
                 <div className={styles['ciea-dash-inner']}>
                     <div className={styles['title']}>
-                    Conheça as Iniciativas do Educom&Clima
+                        Conheça as Iniciativas do Educom&Clima
                     </div>
 
                 </div>
@@ -276,7 +375,7 @@ export default function MapPP() {
 
             <div className={styles.container}>
                 <div className={styles['map-container']}>
-                    <Map center={position} zoomControl={false} zoom={zoom} ref={mapRef} maxZoom={18} minZoom={3} scrollWheelZoom={false}>
+                    <Map center={position} zoomControl={false} zoom={zoom} ref={mapRef} maxZoom={18} minZoom={3} scrollWheelZoom={false} onClick={handleMapClick}>
                         <TileLayer
                             attribution='<a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -288,7 +387,7 @@ export default function MapPP() {
                             format="image/png"
                             transparent={true}
                             opacity={1}
-                            /* cql_filter={iniciativas_ids ? `id in (${iniciativas_ids.join(',')})` : 'id>0'} */
+                            cql_filter={clicked_ufs ? `cd_uf in (${clicked_ufs.join(',')})` : uf_ids ? `cd_uf in (${uf_ids.join(',')})` : 'cd_uf>0'}
                         />
 
                         {!!selected && <WMSTileLayer
@@ -311,52 +410,62 @@ export default function MapPP() {
 
                     <div className={`row ${styles.filters}`}>
 
-                        <div className={styles.title}>
-                            <div></div>
-                            <div>Filtros de Busca</div>
-                        </div>
-
-                        <div className={styles.each}>
-                            <div><Toggler checked={togglers['regioes']} onToggle={(checked) => handleToggle('regioes')(checked)} /></div>
-                            <div>Regiões</div>
-                            <div>
-                                {regioes && (
-                                    <div>
-                                        <StyledReactSelect
-                                            classNamePrefix={reactSelectClassNamePrefix}
-                                            {...selectDefaults}
-                                            onChange={selectedOption => onFilterChange('regioes', selectedOption)}
-                                            closeMenuOnSelect={false}
-                                            components={animatedComponents}
-                                            isMulti
-                                            options={regioes}
-                                            value={fields['regioes']}
-                                        />
-                                    </div>
-                                )}
+                        {tab === 'clicked' && <>
+                            <div className={styles.title}>
+                                <div></div>
+                                <div>Lista de iniciativas selecionadas no mapa</div>
+                                <div className={styles.back2filters} onClick={closeClickedTab}>« voltar aos filtros</div>
                             </div>
-                        </div>
+                        </>}
 
-                        <div className={styles.each}>
-                            <div><Toggler checked={togglers['ufs']} onToggle={(checked) => handleToggle('ufs')(checked)} /></div>
-                            <div>Estado</div>
-                            <div>
-                                {ufs && (
-                                    <div>
-                                        <StyledReactSelect
-                                            classNamePrefix={reactSelectClassNamePrefix}
-                                            {...selectDefaults}
-                                            onChange={selectedOption => onFilterChange('ufs', selectedOption)}
-                                            closeMenuOnSelect={false}
-                                            components={animatedComponents}
-                                            isMulti
-                                            options={ufs}
-                                            value={fields['ufs']}
-                                        />
-                                    </div>
-                                )}
+                        {tab === 'filters' && <>
+                            <div className={styles.title}>
+                                <div></div>
+                                <div>Filtros de Busca</div>
                             </div>
-                        </div>
+
+                            {/* <div className={styles.each}>
+                                <div><Toggler checked={togglers['regioes']} onToggle={(checked) => handleToggle('regioes')(checked)} /></div>
+                                <div>Regiões</div>
+                                <div>
+                                    {regioes && (
+                                        <div>
+                                            <StyledReactSelect
+                                                classNamePrefix={reactSelectClassNamePrefix}
+                                                {...selectDefaults}
+                                                onChange={selectedOption => onFilterChange('regioes', selectedOption)}
+                                                closeMenuOnSelect={false}
+                                                components={animatedComponents}
+                                                isMulti
+                                                options={regioes}
+                                                value={fields['regioes']}
+                                            />
+                                        </div>
+                                    )}
+                                </div>
+                            </div> */}
+
+                            <div className={styles.each}>
+                                <div><Toggler checked={togglers['ufs']} onToggle={(checked) => handleToggle('ufs')(checked)} /></div>
+                                <div>Estado</div>
+                                <div>
+                                    {ufs && (
+                                        <div>
+                                            <StyledReactSelect
+                                                classNamePrefix={reactSelectClassNamePrefix}
+                                                {...selectDefaults}
+                                                onChange={selectedOption => onFilterChange('ufs', selectedOption)}
+                                                closeMenuOnSelect={false}
+                                                components={animatedComponents}
+                                                isMulti
+                                                options={ufs}
+                                                value={fields['ufs']}
+                                            />
+                                        </div>
+                                    )}
+                                </div>
+                            </div>
+                        </>}
 
                         <div className={styles['list-header']}>
                             <div>Iniciativa Selecionadas</div>
@@ -366,14 +475,14 @@ export default function MapPP() {
 
                         {!loading && !!iniciativas && iniciativas.entities.map(p => <div key={p.id} className={styles['list-item']}>
                             <div>{p.nome}</div>
-                            <div>{p.nm_regiao}</div>
+                            <div>{p.regioes?.filter(r => !!r).map(r => r.toUpperCase()).join(',')}</div>
                             <div>
                                 <img onClick={() => handleSelect(p)} src={Mapa} />
-                                <img onClick={() => window.open(`/iniciativa/ciea/${p.id}`, '_blank')} src={Acesso} />
+                                <img onClick={() => window.open(`/iniciativa/educom_clima/${p.id}`, '_blank')} src={Acesso} />
                             </div>
                         </div>)}
 
-                        {loading && [1,2,3,4,5].map(m => <div key={`mock_${m}`} className={`${styles['list-item']} ${styles['mock']}`}>
+                        {loading && [1, 2, 3, 4, 5].map(m => <div key={`mock_${m}`} className={`${styles['list-item']} ${styles['mock']}`}>
                             <div></div>
                             <div></div>
                             <div></div>
