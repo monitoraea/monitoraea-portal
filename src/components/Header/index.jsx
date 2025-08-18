3a83b6c764942eca5712889b794692a72661e8f6
Ricardo Zylbergeld <ricardo@digitao.com.br>
2025-08-18 16:59:30 +0300

    header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,23 +1,25 @@
 import Nav from "../nav/nav";
 
 import bg from "../../images/bg_top.jpg";
-import miolo from "../../images/miolo_top.png";
-import youtube from "../../images/youtube.png";
+import miolo from "../../images/logo_top.png";
 import instagram from "../../images/instagram.png";
+
+import styles from './index.module.scss'
 
 export default function Header() {
   return (
     <>
-      <div className="banner">
-        <img className="bg" src={bg} alt="" />
-        <div className="backdrop"></div>
-        <div className="content">
-          <div className="title">
+      <div className={styles.banner}>
+        <img className={styles.bg} src={bg} alt="" />
+        <div className={styles.backdrop}></div>
+        <div className={styles.content}>
+          <div className={styles.title}>
             <img src={miolo} alt="" />
+            <div className={styles.text}>Sistema Brasileiro de Monitoramento e Avaliação em <strong>Educação Ambiental</strong></div>
           </div>
 
-          <div className="right">
-            <div className="social-media">
+          <div className={styles.right}>
+            <div className={styles['social-media']}>
               <a
                 href="https://www.instagram.com/anppea_monitoraea/"
                 target="_blank"
@@ -27,11 +29,12 @@ export default function Header() {
             </div>
             <button
               onClick={() => (window.location.href = "/colabora")}
-              className="login"
+              className={styles.login}
             >
               Acessar
             </button>
           </div>
+          
         </div>
       </div>
       <Nav />
