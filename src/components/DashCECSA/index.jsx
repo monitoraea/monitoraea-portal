5f43761bfbe5f975e17b28f14afe733a7cf7016a
Ricardo Zylbergeld <ricardo@digitao.com.br>
2025-11-24 17:52:53 +0200

    cecsa - mobile - ok

---
+++ src/components/DashCECSA/index.jsx
@@ -0,0 +1,86 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+
+import { useMediaQuery } from "react-responsive";
+
+import styles from "./styles.module.scss";
+
+export default function MapPP() {
+  const isMobile = useMediaQuery({ maxWidth: 500 });
+
+  const { data: iniciatives } = useQuery(["cne-initiatives"], {
+    queryFn: async () =>
+      (
+        await axios.get(
+          `${import.meta.env.VITE_SERVER}adm/statistics/iniciatives_in_perspectives/cne`,
+        )
+      ).data,
+    staleTime: 3600000,
+  });
+
+  const { data: institutions } = useQuery(["cne-institutions"], {
+    queryFn: async () =>
+      (await axios.get(`${import.meta.env.VITE_SERVER}cne/statistics/cnes`))
+        .data,
+    staleTime: 3600000,
+  });
+
+  const { data: members } = useQuery(["cne-members"], {
+    queryFn: async () =>
+      (
+        await axios.get(
+          `${import.meta.env.VITE_SERVER}gt/perspectives/5/members`,
+        )
+      ).data,
+    staleTime: 3600000,
+  });
+
+  return (
+    <>
+      <section className={styles["cne-dash"]}>
+        <div className="width-limiter">
+          <div className={styles["cne-dash-inner"]}>
+            <div className={styles["title"]}>
+              Conheça os Centros de Educação e Cooperação Socioambiental em
+              atuação no Brasil
+            </div>
+
+            <div className={styles["big-numbers"]}>
+              <div
+                className={`${styles["box-with-image"]} ${isMobile ? "" : styles["large-box"]}`}
+              >
+                <div className={`${styles["box"]} ${styles["box-1"]}`}>
+                  {!iniciatives && <div className={styles.number}>...</div>}
+                  {iniciatives && (
+                    <div className={styles.number}>{iniciatives}</div>
+                  )}
+                  <div className={styles.text}>Centros</div>
+                </div>
+              </div>
+
+              <div className={styles["box-v-group"]}>
+                <div className={styles["box-with-image"]}>
+                  <div className={`${styles["box"]}`}>
+                    {!institutions && <div className={styles.number}>...</div>}
+                    {institutions && (
+                      <div className={styles.number}>{institutions}</div>
+                    )}
+                    <div className={styles.text}>Organizações envolvidas</div>
+                  </div>
+                </div>
+
+                <div className={styles["box-with-image"]}>
+                  <div className={`${styles["box"]}`}>
+                    {!members && <div className={styles.number}>...</div>}
+                    {members && <div className={styles.number}>{members}</div>}
+                    <div className={styles.text}>Pessoas envolvidas</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
--- src/pages/CECSA/CECSA.jsx
+++ src/pages/CECSA/CECSA.jsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
+
+import axios from "axios";
+import { useQuery } from "react-query";
+
 import Header from "../../components/Header";
 
-import DynamicContent from "../../components/DynamicContent";
 import ContentByType from "../../components/ContentByType";
 import Faq from "../../components/Faq";
 
+import { useMediaQuery } from "react-responsive";
+
 import { content_types } from "../../utils";
 
-import MapCECSA from "../../components/MapCECSA";
+import Dash from "../../components/DashCECSA";
+import Map from "../../components/Map";
 
 import download from "../../images/download.png";
 
@@ -14,8 +21,41 @@ import "./style.scss";
 import styles from "./styles.module.scss";
 
 import cecsa from "./cecsa.png";
+import cecsa_mobile from "./cecsa_mobile.png";
 
 function CECSA() {
+  const isMobile = useMediaQuery({ maxWidth: 500 });
+
+  const [filtersString, _filtersString] = useState("");
+
+  const { data: regioes } = useQuery(["regioes"], {
+    queryFn: async () =>
+      (await axios.get(`${import.meta.env.VITE_SERVER}cne/options`)).data,
+    staleTime: 3600000,
+  });
+
+  const { data: ufs } = useQuery(["ufs", { filtersString }], {
+    queryFn: async () =>
+      (
+        await axios.get(
+          `${import.meta.env.VITE_SERVER}cne/ufs?${filtersString}`,
+        )
+      ).data,
+    staleTime: 3600000,
+  });
+
+  const loadNameOptions =
+    (url = "cne/list/") =>
+    (inputValue, callback) => {
+      axios
+        .get(
+          `${import.meta.env.VITE_SERVER}${url}?nome=${inputValue}${filtersString}`,
+        )
+        .then(function ({ data }) {
+          callback(data);
+        });
+    };
+
   return (
     <>
       <Header />
@@ -23,7 +63,7 @@ function CECSA() {
       <div className={styles.logo}>
         <div className="width-limiter">
           <div>
-            <img src={cecsa} />
+            <img src={!isMobile ? cecsa : cecsa_mobile} />
           </div>
         </div>
       </div>
@@ -97,14 +137,15 @@ function CECSA() {
                   onClick={() =>
                     window.open(
                       "https://pppzcm-files.s3.us-east-2.amazonaws.com/Centros.Nucelos.e.Equipamentos.de.Educacao.e.Cooperacao.Socioambiental.pdf",
-                      "_blank"
+                      "_blank",
                     )
                   }
                 >
                   <div className={styles.image}>
                     <img src={download} />
                   </div>{" "}
-                  Baixe o documento de Centros, Núcelos e Equipamentos de Educação e Cooperação Socioambiental
+                  Baixe o documento de Centros, Núcelos e Equipamentos de
+                  Educação e Cooperação Socioambiental
                 </button>
               </div>
             </div>
@@ -112,7 +153,88 @@ function CECSA() {
         </div>
       </section>
 
-      <MapCECSA />
+      <Dash />
+
+      <Map
+        config={{
+          perspective: "cecsa",
+          entity: "cne",
+          geo: {
+            layer: "pppzcm:cecsa",
+            field: "cne_id",
+            cql_field: "cne_id",
+          },
+          resultsTable: {
+            headers: ["Centros Selecionados", "Organização", "Região"],
+            singleUrl: "/iniciativa/cecsa",
+            singleField: "cne_id",
+            data: (results) => [
+              results.nome,
+              results.instituicao_nome,
+              results.nm_regiao,
+            ],
+          },
+          fields: [
+            {
+              key: "regioes",
+              initialFieldState: null,
+              initialToggleState: false,
+              title: "Regiões",
+              type: "select",
+              options: regioes,
+              isMulti: true,
+              reset: ["id", "ufs", "municipios"],
+            },
+            {
+              key: "ufs",
+              initialFieldState: null,
+              initialToggleState: false,
+              title: "Estado",
+              type: "select",
+              options: ufs,
+              isMulti: true,
+              reset: ["id", "municipios"],
+            },
+            {
+              key: "municipios",
+              initialFieldState: null,
+              initialToggleState: false,
+              title: "Município",
+              type: "async_select",
+              options: (inputValue, callback) => {
+                axios
+                  .get(
+                    `${import.meta.env.VITE_SERVER}cne/municipios/?nome=${inputValue}${filtersString}`,
+                  )
+                  .then(function ({ data }) {
+                    callback(data);
+                  });
+              },
+              isMulti: true,
+              reset: ["id"],
+            },
+            {
+              key: "instituicao",
+              initialFieldState: null,
+              initialToggleState: false,
+              title: "Nome da organização",
+              type: "async_select",
+              options: loadNameOptions("cne/instiuicao/list/"),
+              isMulti: true,
+              reset: ["id"],
+            },
+            {
+              key: "id",
+              initialFieldState: null,
+              initialToggleState: false,
+              title: "Nome do centro",
+              type: "async_select",
+              options: loadNameOptions(),
+            },
+          ],
+        }}
+        onFiltersChange={_filtersString}
+      />
 
       {["news" /* ,'learning' */].map((c) => (
         <ContentByType
